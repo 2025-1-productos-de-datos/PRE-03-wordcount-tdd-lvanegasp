c778e81c27fe73315ba1e740fee155db280ee4a4
Lucas Vanegas Palacio <lvanegasp@unal.edu.co>
2025-05-10 10:34:30 -0500

    segundo commit

--- homework/src/_internals/tests/test_internals.py
+++ homework/src/_internals/tests/test_internals.py
@@ -63,12 +63,12 @@ import os
 import shutil
 import sys
 
-from ...count_words import count_words
-from ...parse_args import parse_args
-from ...preprocess_lines import preprocess_lines
-from ...read_all_lines import read_all_lines
-from ...split_into_words import split_into_words
-from ...write_word_counts import write_word_counts
+from ..count_words import count_words
+from ..parse_args import parse_args
+from ..preprocess_lines import preprocess_lines
+from ..read_all_lines import read_all_lines
+from ..split_into_words import split_into_words
+from ..write_word_counts import write_word_counts
 
 
 def test_parse_args():
--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -2,12 +2,12 @@
 # python3 -m homework --input data/input --output data/output
 
 
+from homework.src._internals.count_words import count_words
 from homework.src._internals.parse_args import parse_args
+from homework.src._internals.preprocess_lines import preprocess_lines
 from homework.src._internals.read_all_lines import read_all_lines
-from homework.src.count_words import count_words
-from homework.src.preprocess_lines import preprocess_lines
-from homework.src.split_into_words import split_into_words
-from homework.src.write_word_counts import write_word_counts
+from homework.src._internals.split_into_words import split_into_words
+from homework.src._internals.write_word_counts import write_word_counts
 
 
 def main():
